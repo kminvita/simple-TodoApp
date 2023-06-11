bae137ba75b0870abdf98a8342f9dcca0ac7681c
kminvita <vita_mink@daum.net>
2023-06-11 14:56:06 +0900

    할일 목록 타이틀 작성

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ export default class App extends Component {
     return (
       <div className="container">
         <div className="todoBlock">
-
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
         </div>
       </div>
     )
