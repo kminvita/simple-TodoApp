5e89d2b7b00f917c584cad697130f1e045867b80
kminvita <vita_mink@daum.net>
2023-06-11 17:16:30 +0900

    할일 입력 UI구현

--- src/App.js
+++ src/App.js
@@ -56,6 +56,21 @@ export default class App extends Component {
             <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
           </div>
           ))}
+          <form style={{display: "flex"}}>
+            <input 
+              type="text" 
+              name="value" 
+              style={{flex:"10", padding: "5px"}} 
+              placeholder="해야 할 일을 입력하세요"
+              value=""
+              />
+            <input 
+              type="submit" 
+              value="입력"
+              className="btn"
+              style={{flex: "1"}}
+            />
+          </form>
         </div>
       </div>
     )
