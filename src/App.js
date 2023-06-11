90dae313de6e6999b26c930945229f9f8b3b03d9
kminvita <vita_mink@daum.net>
2023-06-11 14:53:46 +0900

    개발환경 세팅 및 container, todo component 생성

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+
+        </div>
+      </div>
+    )
+  }
+}
